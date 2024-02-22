ee5928c642d430ecd5a5737bda1903811715e907
akira <33633391+hskwakr@users.noreply.github.com>
2024-02-22 12:21:32 +0900

    For versioning API

--- projects/envato-tuts/build-rest-api-with-laravel/laravel-api/app/Http/Controllers/Api/v1/CustomerController.php
+++ projects/envato-tuts/build-rest-api-with-laravel/laravel-api/app/Http/Controllers/Api/v1/CustomerController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\V1;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Models\Customer;
