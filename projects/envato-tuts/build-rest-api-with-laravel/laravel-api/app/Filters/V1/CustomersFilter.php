6c710cc11aa54f59c5dcffe0b06297524101af89
akira <33633391+hskwakr@users.noreply.github.com>
2024-03-03 11:48:10 +0900

    ApiFilter

--- projects/envato-tuts/build-rest-api-with-laravel/laravel-api/app/Filters/V1/CustomersFilter.php
+++ projects/envato-tuts/build-rest-api-with-laravel/laravel-api/app/Filters/V1/CustomersFilter.php
@@ -2,9 +2,9 @@
 
 namespace App\Filters\V1;
 
-use Illuminate\Http\Request;
+use App\Filters\ApiFilter;
 
-class CustomersFilter
+class CustomersFilter extends ApiFilter
 {
     protected $safeParams = [
         'id' => ['eq'],
@@ -28,26 +28,4 @@ class CustomersFilter
         'gt' => '>',
         'gte' => '>=',
     ];
-
-    public function transform(Request $request)
-    {
-        $eloQuery = [];
-
-        foreach ($this->safeParams as $param => $operators) {
-            $query = $request->query($param);
-            if (!isset($query)) {
-                continue;
-            }
-
-            $column = $this->columnMap[$param] ?? $param;
-
-            foreach ($operators as $operator) {
-                if (isset($query[$operator])) {
-                    $eloQuery[] = [$column, $this->operatorMap[$operator], $query[$operator]];
-                }
-            }
-        }
-
-        return $eloQuery;
-    }
 }
--- projects/envato-tuts/build-rest-api-with-laravel/laravel-api/tests/Unit/ApiFilterTest.php
+++ projects/envato-tuts/build-rest-api-with-laravel/laravel-api/tests/Unit/ApiFilterTest.php
@@ -2,66 +2,81 @@
 
 namespace Tests\Unit;
 
-use App\Filters\V1\CustomersFilter;
+use App\Filters\ApiFilter;
 use Illuminate\Http\Request;
 use PHPUnit\Framework\TestCase;
 
-class CustomersFilterTest extends TestCase
+class ApiFilterTest extends TestCase
 {
+    protected $apiFilter;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->apiFilter = new class extends ApiFilter
+        {
+            protected $safeParams = [
+                'name' => ['eq'],
+                'postalCode' => ['gt', 'lt']
+            ];
+
+            protected $columnMap = [
+                'postalCode' => 'postal_code'
+            ];
+
+            protected $operatorMap = [
+                'eq' => '=',
+                'gt' => '>',
+                'lt' => '<',
+            ];
+        };
+    }
+
     /** 
      * @test
      */
     public function transformが正しいクエリを返す()
     {
-        // モックRequestを作成
         $request = Request::create('/', 'GET', [
             'name' => ['eq' => 'John Doe'],
             'postalCode' => ['gt' => '1000', 'lt' => '2000'],
         ]);
 
-        // CustomerQueryサービスをインスタンス化
-        $customerQuery = new CustomersFilter();
+        $result = $this->apiFilter->transform($request);
 
-        // transformメソッドを実行
-        $result = $customerQuery->transform($request);
-
-        // 期待される結果を定義
         $expected = [
             ['name', '=', 'John Doe'],
             ['postal_code', '>', '1000'],
             ['postal_code', '<', '2000'],
         ];
 
-        // 結果が期待通りか検証
         $this->assertEquals($expected, $result);
     }
 
     /** 
      * @test
-     * @group transform
      */
     public function transformにパラメータを渡さなかったら空のクエリを返す()
     {
         $request = Request::create('/', 'GET');
-        $customerQuery = new CustomersFilter();
-        $result = $customerQuery->transform($request);
+
+        $result = $this->apiFilter->transform($request);
 
         $this->assertEmpty($result);
     }
 
     /** 
      * @test
-     * @group transform
      */
     public function transformがサポートされていないパラメータを無視する()
     {
         $request = Request::create('/', 'GET', [
-            'unsupportedParam1' => ['eq' => 'value'],
+            'unsupported' => ['eq' => 'value'],
             'name' => ['eq' => 'John Doe'],
-            'unsupportedParam2' => ['eq' => 'value'],
         ]);
-        $customerQuery = new CustomersFilter();
-        $result = $customerQuery->transform($request);
+
+        $result = $this->apiFilter->transform($request);
 
         $expected = [
             ['name', '=', 'John Doe'],
