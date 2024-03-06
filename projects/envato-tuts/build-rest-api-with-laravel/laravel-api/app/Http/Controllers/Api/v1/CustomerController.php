5acdbe649b94b6e1e0441afde0adcd2871e9e037
akira <33633391+hskwakr@users.noreply.github.com>
2024-03-06 09:07:00 +0900

    4.1 Creating Resources With POST Requests

--- projects/envato-tuts/build-rest-api-with-laravel/laravel-api/app/Http/Controllers/Api/v1/CustomerController.php
+++ projects/envato-tuts/build-rest-api-with-laravel/laravel-api/app/Http/Controllers/Api/v1/CustomerController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Resources\V1\CustomerResource;
 use App\Models\Customer;
 use App\Filters\V1\CustomersFilter;
+use App\Http\Requests\V1\StoreCustomerRequest;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
@@ -45,7 +45,7 @@ class CustomerController extends Controller
      */
     public function store(StoreCustomerRequest $request)
     {
-        //
+        return new CustomerResource(Customer::create($request->all()));
     }
 
     /**
