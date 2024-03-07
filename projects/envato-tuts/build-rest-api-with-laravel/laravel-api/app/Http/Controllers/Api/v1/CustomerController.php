045e7db942da1d6f44298c10375ddfb468a69772
akira <33633391+hskwakr@users.noreply.github.com>
2024-03-07 18:35:03 +0900

    4.2 Updating With PUT and PATCH

--- projects/envato-tuts/build-rest-api-with-laravel/laravel-api/app/Http/Controllers/Api/v1/CustomerController.php
+++ projects/envato-tuts/build-rest-api-with-laravel/laravel-api/app/Http/Controllers/Api/v1/CustomerController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Resources\V1\CustomerResource;
 use App\Models\Customer;
 use App\Filters\V1\CustomersFilter;
 use App\Http\Requests\V1\StoreCustomerRequest;
+use App\Http\Requests\V1\UpdateCustomerRequest;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
@@ -74,7 +74,7 @@ class CustomerController extends Controller
      */
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        //
+        $customer->update($request->all());
     }
 
     /**
