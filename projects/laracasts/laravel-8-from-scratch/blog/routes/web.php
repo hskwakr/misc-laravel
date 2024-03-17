a9a4f39fb9a791fd59487b99c5f678ab685fa681
akira <33633391+hskwakr@users.noreply.github.com>
2024-03-17 19:57:21 +0900

    ep10

--- projects/laracasts/laravel-8-from-scratch/blog/routes/web.php
+++ projects/laracasts/laravel-8-from-scratch/blog/routes/web.php
@@ -5,14 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('posts');
 });
+
 Route::get('/posts/{post}', function ($slug) {
-    $path = __DIR__ . '/../resources/posts/' . $slug . '.html';
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
 
     if (!file_exists($path)) {
         return redirect('/');
     }
 
-    $post = file_get_contents($path);
+    $post = cache()->remember("posts.{$slug}", now()->addMinutes(20), function () use ($path) {
+        return file_get_contents($path);
+    });
 
     return view('post', [
         'post' => $post,
