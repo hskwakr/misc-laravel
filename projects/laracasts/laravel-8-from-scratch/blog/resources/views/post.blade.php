9b62b30c0d92b0c1f3a07af68283d49df86e7753
akira <33633391+hskwakr@users.noreply.github.com>
2024-03-17 15:09:06 +0900

    ep 1 - 7

---
+++ projects/laracasts/laravel-8-from-scratch/blog/resources/views/post.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Blog</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+
+<body>
+    <article>
+        <h1><a href="/post">First Post</a></h1>
+
+        <p>
+            Let's embark on a captivating journey through a serene forest, where the whispering wind carries tales of ancient wisdom. Amidst towering trees, their leaves a vibrant tapestry of greens, a narrow path weaves its way, untouched by time. Sunlight filters through the canopy, creating a dance of light and shadow, a spectacle of nature's artistry. Along this path, a gentle stream flows, its crystal waters singing songs of tranquility. Birds, with their melodious chirps, accompany the wanderer, offering a symphony of the wild. In this realm, where nature reigns supreme, every step unveils the beauty of simplicity, a reminder of the world's timeless grace.
+        </p>
+
+    </article>
+
+    <a href="/">Go Back</a>
+</body>
+
+</html>
