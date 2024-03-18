e7b59b81a2bdb3a27878c129661ab6226165684e
akira <33633391+hskwakr@users.noreply.github.com>
2024-03-18 22:28:09 +0900

    ep15

--- projects/laracasts/laravel-8-from-scratch/blog/resources/views/post.blade.php
+++ projects/laracasts/laravel-8-from-scratch/blog/resources/views/post.blade.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Blog</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-    <!-- Styles -->
-    <link rel="stylesheet" href="/css/app.css">
-</head>
-
-<body>
+<x-layout>
     <article>
         <h1>
             {{ $post->title }}
@@ -25,6 +8,4 @@
     </article>
 
     <a href="/">Go Back</a>
-</body>
-
-</html>
+</x-layout>
