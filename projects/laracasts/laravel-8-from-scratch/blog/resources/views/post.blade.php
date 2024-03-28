777b590cfe6afb6f685ecec61fcff85e7217fbc8
akira <33633391+hskwakr@users.noreply.github.com>
2024-03-28 23:19:06 +0900

    ep25

--- projects/laracasts/laravel-8-from-scratch/blog/resources/views/post.blade.php
+++ projects/laracasts/laravel-8-from-scratch/blog/resources/views/post.blade.php
@@ -5,7 +5,7 @@
         </h1>
 
         <p>
-            <a href="#">
+            <a href="/categories/{{ $post->category->slug }}">
                 {{ $post->category->name }}
             </a>
         </p>
