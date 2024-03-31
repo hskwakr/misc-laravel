99736dd7e0d47bb04374b9217f21076a8b9503c2
akira <33633391+hskwakr@users.noreply.github.com>
2024-03-31 14:59:23 +0900

    ep27

--- projects/laracasts/laravel-8-from-scratch/blog/database/seeders/DatabaseSeeder.php
+++ projects/laracasts/laravel-8-from-scratch/blog/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +15,52 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::truncate();
+        Post::truncate();
+        Category::truncate();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal',
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work',
+        ]);
+
+        $hobby = Category::create([
+            'name' => 'Hobby',
+            'slug' => 'hobby',
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $personal->id,
+            'title' => 'My Personal Post',
+            'slug' => 'my-personal-post',
+            'excerpt' => 'This is excerpt',
+            'body' => 'This is body'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => 'This is excerpt',
+            'body' => 'This is body'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $hobby->id,
+            'title' => 'My Hobby Post',
+            'slug' => 'my-hobby-post',
+            'excerpt' => 'This is excerpt',
+            'body' => 'This is body'
         ]);
     }
 }
