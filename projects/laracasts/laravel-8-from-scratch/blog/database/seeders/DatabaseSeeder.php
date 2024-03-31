ee14d9e7307cf467d4c6c3efbbc4da5596d94bab
akira <33633391+hskwakr@users.noreply.github.com>
2024-04-01 00:46:24 +0900

    ep28

--- projects/laracasts/laravel-8-from-scratch/blog/database/seeders/DatabaseSeeder.php
+++ projects/laracasts/laravel-8-from-scratch/blog/database/seeders/DatabaseSeeder.php
@@ -15,52 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
-
         $user = User::factory()->create();
 
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal',
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work',
-        ]);
-
-        $hobby = Category::create([
-            'name' => 'Hobby',
-            'slug' => 'hobby',
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My Personal Post',
-            'slug' => 'my-personal-post',
-            'excerpt' => 'This is excerpt',
-            'body' => 'This is body'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => 'This is excerpt',
-            'body' => 'This is body'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $hobby->id,
-            'title' => 'My Hobby Post',
-            'slug' => 'my-hobby-post',
-            'excerpt' => 'This is excerpt',
-            'body' => 'This is body'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
     }
 }
