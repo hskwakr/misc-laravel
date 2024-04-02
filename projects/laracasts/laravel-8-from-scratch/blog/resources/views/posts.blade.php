a947033d1aa0be2f7e2e83cd7d2d8b2fcb15f406
akira <33633391+hskwakr@users.noreply.github.com>
2024-04-02 22:24:26 +0900

    ep32

--- projects/laracasts/laravel-8-from-scratch/blog/resources/views/posts.blade.php
+++ projects/laracasts/laravel-8-from-scratch/blog/resources/views/posts.blade.php
@@ -1,37 +1,17 @@
 <x-layout>
-    <!-- @foreach ($posts as $post)
-    <article>
-        <h1>
-            <a href="/posts/{{ $post->slug }}">
-                {{ $post->title }}
-            </a>
-        </h1>
-
-        <p>
-            By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in
-            <a href="/categories/{{ $post->category->slug }}">
-                {{ $post->category->name }}
-            </a>
-        </p>
-
-        {{ $post->excerpt }}
-    </article>
-    @endforeach -->
-
     @include ('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card />
+        @if ($posts->count())
 
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card />
-            <x-post-card />
-        </div>
+        <x-posts-grid :posts="$posts" />
 
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card />
-            <x-post-card />
-            <x-post-card />
-        </div>
+        @else
+
+        <p class="text-center">
+            No posts yet. Please check back later.
+        </p>
+
+        @endif
     </main>
 </x-layout>
