6d62a2eafa08331a8c5e443083091213cf0aab61
akira <33633391+hskwakr@users.noreply.github.com>
2024-05-16 18:15:59 +0900

    2

--- projects/laravel-daily/build-laravel-api-step-by-step/project01/routes/api.php
+++ projects/laravel-daily/build-laravel-api-step-by-step/project01/routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\Auth;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('auth/register', Auth\RegisterController::class);
