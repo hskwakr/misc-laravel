237a15de2c50e789b77f8318dcccf7350e39633a
akira <33633391+hskwakr@users.noreply.github.com>
2024-05-19 17:48:12 +0900

    3

--- projects/laravel-daily/build-laravel-api-step-by-step/project01/routes/api.php
+++ projects/laravel-daily/build-laravel-api-step-by-step/project01/routes/api.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\Auth;
 
 Route::post('auth/register', Auth\RegisterController::class);
+Route::post('auth/login', Auth\LoginController::class);
