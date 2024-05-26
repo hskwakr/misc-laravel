ea0933e4fa37aed947a81311ff1937d44bbc5eca
akira <33633391+hskwakr@users.noreply.github.com>
2024-05-26 23:50:40 +0900

    4

--- projects/laravel-daily/build-laravel-api-step-by-step/project01/routes/api.php
+++ projects/laravel-daily/build-laravel-api-step-by-step/project01/routes/api.php
@@ -5,3 +5,12 @@ use App\Http\Controllers\Api\V1\Auth;
 
 Route::post('auth/register', Auth\RegisterController::class);
 Route::post('auth/login', Auth\LoginController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('auth/logout', Auth\LogoutController::class);
+
+    Route::get('profile', [Auth\ProfileController::class, 'show']);
+    Route::put('profile', [Auth\ProfileController::class, 'update']);
+
+    Route::put('password', Auth\PasswordUpdateController::class);
+});
